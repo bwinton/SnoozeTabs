7d0bc6993ab48ac6e34375654d3f15f884e5adde
Les Orchard <me@lmorchard.com>
2016-12-16 15:20:56 -0500

    fix: Constrain date/time selection to future
    * Disable past dates in calendar selector
    * Disable past hours/minutes/seconds in time selector
    * Disable "snooze" button if the time selected is in the past
    * Properly handle calendar month / year change
    * Properly update time selector with the current value
    * Show seconds in the time selector
    * Ensure wake timer never gets set earlier than 5 seconds from now
    
    Fixes #50.

--- src/lib/components/DatePickerPanel.js
+++ src/lib/components/DatePickerPanel.js
@@ -9,7 +9,10 @@ import TimePickerPanel from 'rc-time-picker/lib/Panel';
 export default class DatePickerPanel extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { currentValue: props.defaultValue };
+    this.state = {
+      currentValue: props.defaultValue,
+      confirmDisabled: false
+    };
   }
 
   componentWillReceiveProps(nextProps) {
@@ -17,9 +20,8 @@ export default class DatePickerPanel extends React.Component {
   }
 
   render() {
-    const { id, header, active, onClose, onSelect } = this.props;
-    const { currentValue } = this.state;
-    const timePickerElement = <TimePickerPanel />;
+    const { id, header, active, onClose } = this.props;
+    const { currentValue, confirmDisabled } = this.state;
 
     return (
       <div id={id} className={classnames('panel', { active })}>
@@ -27,19 +29,85 @@ export default class DatePickerPanel extends React.Component {
         <Calendar showOk={false}
                   showDateInput={false}
                   value={currentValue}
-                  timePicker={timePickerElement}
-                  onSelect={value => this.handleTimeSelect(value)} />
+                  timePicker={(
+                    <TimePickerPanel value={currentValue}
+                      showHour={true} showMinute={true} showSecond={true} />
+                  )}
+                  disabledDate={this.disabledDate.bind(this)}
+                  disabledTime={this.disabledTime.bind(this)}
+                  onChange={value => this.handleChange(value)}
+                  onSelect={value => this.handleChange(value)} />
         <div className="footer">
           <div className="back"
                onClick={onClose}><span>« Back</span></div>
-          <div className="confirm snooze"
-               onClick={() => onSelect(this.state.currentValue)}><span>Snooze!</span></div>
+          <div className={classnames('confirm', 'snooze', { disabled: confirmDisabled })}
+               title={confirmDisabled ? 'Time selection is in the past' : ''}
+               onClick={() => this.handleConfirm()}><span>Snooze!</span></div>
         </div>
       </div>
     );
   }
 
-  handleTimeSelect(value) {
-    this.setState({ currentValue: value });
+  disabledDate(current) {
+    if (!current) { return false; }
+    const today = moment().hour(0).minute(0).second(0);
+    return current.valueOf() < today.valueOf();
+  }
+
+  makeRangeArray(start, end) {
+    const out = [];
+    for (let idx = start; idx < end; idx++) {
+      out.push(idx);
+    }
+    return out;
+  }
+
+  disabledTime() {
+    const { currentValue } = this.state;
+    if (!currentValue) { return; }
+
+    // All time selection disabled for past dates
+    const today = moment().hour(0).minute(0).second(0);
+    if (currentValue.valueOf() < today.valueOf()) {
+      return {
+        disabledHours: () => this.makeRangeArray(0, 24),
+        disabledMinutes: () => this.makeRangeArray(0, 60),
+        disabledSeconds: () => this.makeRangeArray(0, 60)
+      }
+    }
+
+    // Disable past times for today as appropriate
+    const now = moment();
+    if (now.date() === currentValue.date() &&
+        now.month() === currentValue.month() &&
+        now.year() === currentValue.year()) {
+      return {
+        disabledHours: () =>
+          this.makeRangeArray(0, now.hour()),
+        disabledMinutes: () =>
+          now.hour() === currentValue.hour() ?
+            this.makeRangeArray(0, now.minute()) : [],
+        disabledSeconds: () =>
+          now.hour() === currentValue.hour() &&
+          now.minute() === currentValue.minute() ?
+            this.makeRangeArray(0, now.second()) : []
+      };
+    }
+  }
+
+  handleChange(value) {
+    this.setState({
+      currentValue: value,
+      confirmDisabled: (value.valueOf() <= Date.now())
+    });
+  }
+
+  handleConfirm() {
+    const { onSelect } = this.props;
+    const { currentValue } = this.state;
+
+    if (currentValue.valueOf() > Date.now()) {
+      onSelect(currentValue);
+    }
   }
 }
