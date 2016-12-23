938a207714da70645683b5319811b7742672be97
Les Orchard <me@lmorchard.com>
2016-12-23 17:28:44 -0500

    chore: Initial test framework setup
    * Add mocha, chai, and sinon dependencies
    * Add test runner to build & watch tasks
    * Initial empty module for metrics & tests
    * Add `test/.setup.js` for test bootstrap code

---
+++ test/lib/metrics-test.js
@@ -0,0 +1,10 @@
+import { expect } from 'chai';
+import sinon from 'sinon';
+
+import Metrics from '../../src/lib/metrics';
+
+describe('Metrics', () => {
+  it('should exist', () => {
+    expect(true).to.be.true;
+  });
+});
