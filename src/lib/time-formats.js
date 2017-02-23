398110a6bd8f8e5baef65e0c8422c3fc219418a1
Francesco Lodolo (:flod) <flodolo@mozilla.com>
2017-02-23 07:24:44 +0100

    Add formats for all supported languages

--- src/lib/time-formats.js
+++ src/lib/time-formats.js
@@ -6,9 +6,39 @@ export const formats = {
     short_date_time: 'ddd [@] ha',
     long_date_time: 'ddd MMM D \ [@] ha'
   },
-  'it': {
+  'cs,de,dsb,hsb,hu,sk': {
+    short_time: '[@] H',
+    short_date_time: 'ddd [@] H',
+    long_date_time: 'ddd D. MMM \ [@] H'
+  },
+  'es,pt': {
+    short_time: '[@] H',
+    short_date_time: 'ddd [@] H',
+    long_date_time: 'ddd D [de] MMM \ [@] H'
+  },
+  'fr,fy-NL,it,nl,ru,sv,tr': {
     short_time: '[@] H',
     short_date_time: 'ddd [@] H',
     long_date_time: 'ddd D MMM \ [@] H'
+  },
+  'he': {
+    short_time: '[@] H',
+    short_date_time: 'ddd [@] H',
+    long_date_time: 'ddd D [ב]MMM \ [@] H'
+  },
+  'ja': {
+    short_time: '[@] ah',
+    short_date_time: 'ddd [@] ah',
+    long_date_time: 'M月D日 ddd \ [@] ah'
+  },
+  'zh-CN': {
+    short_time: '[@] ah',
+    short_date_time: 'ddd [@] ah',
+    long_date_time: 'MMMD日ddd \ [@] ah'
+  },
+  'zh-TW': {
+    short_time: '[@] ah',
+    short_date_time: 'ddd [@] ah',
+    long_date_time: 'MMMD日ddd \ [@] ah'
   }
 };
