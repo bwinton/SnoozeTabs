c1a6b1cabd3161cdcea16772fd72d1a00625566d
Les Orchard <me@lmorchard.com>
2017-04-12 16:38:27 -0400

    Improve l10n on DatePickerPanel component
    - Patch Moment.format() to use Intl.DateTimeFormat() API for rc-calendar
      locale formatters in DatePickerPanel
    
    - Upgrade rc-calendar component to 8.1.0
    
    - Simplify l10n code in DatePickerPanel to only use strings from the
      extension's own locales, rather than trying to use rc-calendar locales
    
    - Drop rc-calendar strings that don't seem reachable from our use case
      of the component
    
    - Add more detailed comments before each of the remaining new strings
    
    - Small bugfix for storybook
    
    - Small bugfix for initially null datetime on "Pick" time option (#304)
    
    Fixes #305.
    Fixes #304.

--- src/lib/components/DatePickerPanel.js
+++ src/lib/components/DatePickerPanel.js
@@ -5,53 +5,42 @@ import classnames from 'classnames';
 import 'moment/min/locales.min';
 import Calendar from 'rc-calendar';
 import TimePicker from 'rc-time-picker';
-import { use12hFormat } from '../time-formats.js';
+import { getLocalizedDateTime, use12hFormat } from '../time-formats.js';
 
-import cs_CZ from 'rc-calendar/lib/locale/cs_CZ';
-import da_DK from 'rc-calendar/lib/locale/da_DK';
-import de_DE from 'rc-calendar/lib/locale/de_DE';
 import en_US from 'rc-calendar/lib/locale/en_US';
-import ja_JP from 'rc-calendar/lib/locale/ja_JP';
-import pl_PL from 'rc-calendar/lib/locale/pl_PL';
-import pt_BR from 'rc-calendar/lib/locale/pt_BR';
-import ru_RU from 'rc-calendar/lib/locale/ru_RU';
-import zh_CN from 'rc-calendar/lib/locale/zh_CN';
 
 const uiLanguage = browser.i18n.getUILanguage();
 const momentLocale = uiLanguage.replace('_', '-');
-const rcCalendarLocales = { cs_CZ, da_DK, de_DE, en_US, ja_JP, pl_PL, pt_BR, ru_RU, zh_CN };
-const rcCalendarLocale = (uiLanguage in rcCalendarLocales) ? rcCalendarLocales[uiLanguage] : {};
+
+// Set up functional formatters using IntlDateTimeFormat API
+const makeFormatter = type => time => getLocalizedDateTime(time, type);
+const calendarLocale = {
+  yearFormat: makeFormatter('year'),
+  dateFormat: makeFormatter('date'),
+  dayFormat:  makeFormatter('day'),
+  dateTimeFormat: makeFormatter('dateTime'),
+  monthFormat: makeFormatter('month'),
+};
+
+// HACK: This is kind of ugly, but rc-calendar doesn't use an easily patchable
+// formatter for the month & year in the header of the calendar control [1][2]
+// [1] https://github.com/react-component/calendar/blob/354fb3cfb7501cba1dbc607271abceb33deb72c6/src/calendar/CalendarHeader.jsx#L89
+// [2] https://github.com/react-component/calendar/blob/354fb3cfb7501cba1dbc607271abceb33deb72c6/src/calendar/CalendarHeader.jsx#L117
+calendarLocale.monthBeforeYear = new Intl.DateTimeFormat(
+  [uiLanguage.replace('_', '-'), 'en-US'],
+  {month: 'numeric', year: 'numeric'}
+).formatToParts()[0].type === 'month';
+
+// Assemble a locale for rc-calendar from the extension's strings, with the
+// en_US locale from rc-calendar as a fallback
 const localeKeys = [
   'today', 'now', 'backToToday', 'ok', 'clear', 'month', 'year', 'timeSelect',
-  'dateSelect', 'monthSelect', 'yearSelect', 'decadeSelect', 'yearFormat',
-  'dateFormat', 'dayFormat', 'dateTimeFormat', 'monthFormat',
+  'dateSelect', 'monthSelect', 'yearSelect', 'decadeSelect',
   'monthBeforeYear', 'previousMonth', 'nextMonth', 'previousYear', 'nextYear',
   'previousDecade', 'nextDecade', 'previousCentury', 'nextCentury'
 ];
-
-// Assemble calendarLocale from a combination of strings supplied by
-// rc-calendar and by Pontoon localizers, which ever seems better.
-const calendarLocale = {};
-localeKeys.forEach(key => {
-  const defaultString = rcCalendarLocales.en_US[key];
-  const rcCalendarString = rcCalendarLocale[key];
-  const localString = browser.i18n.getMessage(`calendar_${key}`);
-
-  let val;
-  if (localString === defaultString && rcCalendarString && rcCalendarString !== defaultString) {
-    // If the locally translated string is the same as the default, it's
-    // possibly a fallback. If we have a string from rc-calendar that is not
-    // also the default, let's use it.
-    val = rcCalendarString;
-  } else if (localString) {
-    // Otherwise, use the localString if it's not empty
-    val = localString;
-  } else {
-    // Finally fall back to default.
-    val = defaultString;
-  }
-  calendarLocale[key] = val;
-});
+localeKeys.forEach(key =>
+  calendarLocale[key] = browser.i18n.getMessage(`calendar_${key}`) || en_US[key]);
 
 // Arbitrary 0.5s interval for live validation of time selection
 const VALIDATION_INTERVAL = 500;
@@ -61,7 +50,7 @@ export default class DatePickerPanel extends React.Component {
     super(props);
     this.validationTimer = null;
     this.state = {
-      currentValue: props.defaultValue,
+      currentValue: props.defaultValue || props.moment(),
       confirmDisabled: false
     };
   }
@@ -84,7 +73,11 @@ export default class DatePickerPanel extends React.Component {
     const { currentValue, confirmDisabled } = this.state;
     const disabledTimeFns = this.disabledTime();
 
+    // Clone & patch the moment value to accept functions as formats
     const currentValueLocalized = currentValue.clone().locale(momentLocale);
+    const origFormat = currentValueLocalized.format;
+    currentValueLocalized.format = format => (typeof format === 'function') ?
+      format(currentValueLocalized) : origFormat.call(currentValueLocalized, format);
 
     const timeFormat = use12hFormat ? 'h:mm a' : 'HH:mm';
 
